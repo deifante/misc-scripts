0a6c72fbe4e9d66c322e5eed8bc3bec3a36cd8d1
Deifante Jay Walters <deifante@gmail.com>
2015-09-14 13:40:57 -0600

    still slimmer

--- highcharts-chart-request.php
+++ highcharts-chart-request.php
@@ -1,14 +1,19 @@
 <?php
 $curl_handle = curl_init("https://export.highcharts.com");
 $form_data = array(
-    'options' => "{xAxis: {categories: ['Jan', 'Feb', 'Mar', 'Apr']},series: [{data: [29.9, 71.5, 106.4, 141.3]}]}",
-    'type' => 'image/png',
-    'width' => null,
-    'scale' => null,
-    'constr' => 'Chart',
-    'callback' => null
+    'options' => "{xAxis: {categories: ['Jan', 'Feb', 'Mar', 'Avril']},series: [{data: [29.9, 71.5, 106.4, 141.3]}]}",
+    'type' => 'image/png'
 );
+
+curl_setopt($curl_handle, CURLOPT_VERBOSE, true);
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $form_data);
-curl_exec($curl_handle);
+$image_data = curl_exec($curl_handle);
+if (false !== $image_data) {
+    $file_handle = fopen("lol.png", "w");
+    $result = fwrite($file_handle, $image_data);
+    fclose($file_handle);
+}
 curl_close($curl_handle);
+
 echo "Done\n";
